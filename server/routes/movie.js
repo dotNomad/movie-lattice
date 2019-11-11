a13cb904941d38b94e0f4ab5369521fc5ba91ce5
Jordan Jensen <jordan@dotnomad.com>
2019-11-11 13:20:40 -0800

    Add /movie/:id route to server

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -3,9 +3,18 @@ const fetch = require('node-fetch');
 const config = require('../config.json');
 const router = express.Router();
 
+const apiKey = config.apiKey;
+
 router.get('/popular', (req, res) => {
-    const apiKey = config.apiKey;
     const uri = new URL(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => res.json(data));
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const uri = new URL(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`)
     fetch(uri)
         .then(response => response.json())
         .then(data => res.json(data));
