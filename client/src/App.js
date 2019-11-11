c36959428ec114365c4a69df532f98454e0bab2d
Jordan Jensen <jordan@dotnomad.com>
2019-11-11 12:54:34 -0800

    Create search form for main page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Movie Lattice</h1>
       <PopularMovies />
     </div>
   );
--- client/src/components/PopularMovies/PopularMovies.js
+++ client/src/components/PopularMovies/PopularMovies.js
@@ -4,18 +4,49 @@ import MovieGrid from '../MovieGrid/MovieGrid';
 class PopularMovies extends Component {
     constructor(props) {
         super(props);
-        this.state = {movies: []};
+        this.state = {movies: [], searchValue: ''};
+    }
+
+    handleChange = (event) => {
+        this.setState({searchValue: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const query = encodeURI(this.state.searchValue);
+        if (query) {
+            fetch(`/search/movie?query=${query}`)
+                .then(response => response.json())
+                .then(data => this.setState({ movies: data.results }));
+        } else {
+            this.showPopularMovies();
+        }
     }
 
     componentDidMount() {
+        this.showPopularMovies();
+    }
+
+    showPopularMovies = () => {
         fetch('/movie/popular')
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.results }))
+            .then(data => this.setState({ movies: data.results }));
     }
 
     render() {
         return (
-            <MovieGrid movies={this.state.movies}/>
+            <div className="App">
+                <h1>Movie Lattice</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Search:
+                        <input type="text" value={this.state.searchValue} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <MovieGrid movies={this.state.movies}/>
+            </div>
         );
     }
 }
