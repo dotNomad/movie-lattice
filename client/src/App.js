8729c74caae1d1174dac37212e4c2ae261047764
Jordan Jensen <jordan@dotnomad.com>
2019-11-11 13:53:38 -0800

    Use max-width for the entire app

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Switch>
           <Route exact={true} path="/">
             <PopularMovies />
