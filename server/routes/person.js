3174f8ed2cecb293ada57ad9da6e85f0d62b3c00
Jordan Jensen <jordan@dotnomad.com>
2019-11-11 16:29:16 -0800

    Add person/:id/images and movie_credits to server

--- server/routes/person.js
+++ server/routes/person.js
@@ -13,4 +13,20 @@ router.get('/:id', (req, res) => {
         .then(data => res.json(data));
 });
 
+router.get('/:id/images', (req, res) => {
+    const { id } = req.params;
+    const uri = new URL(`https://api.themoviedb.org/3/person/${id}/images?api_key=${apiKey}`);
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => res.json(data));
+});
+
+router.get('/:id/movie_credits', (req, res) => {
+    const { id } = req.params;
+    const uri = new URL(`https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${apiKey}`);
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => res.json(data));
+});
+
 module.exports = router;
