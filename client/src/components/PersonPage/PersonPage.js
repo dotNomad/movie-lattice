218849dde55cf7fb2f26b5af14ec21038a483db6
Jordan Jensen <jordan@dotnomad.com>
2019-11-11 16:42:45 -0800

    Flesh out PersonPage
    - Add image, biography, and movie listings

--- client/src/components/PersonPage/PersonPage.js
+++ client/src/components/PersonPage/PersonPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { movieDBImgSource } from '../../utils/Utils';
 
 import './PersonPage.css';
@@ -6,7 +7,7 @@ import './PersonPage.css';
 class PersonPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { data: {} }
+        this.state = { data: {}, images: [], movie_credits: [] }
     }
 
     componentDidMount() {
@@ -14,15 +15,41 @@ class PersonPage extends Component {
         fetch(`/person/${id}`)
             .then(response => response.json())
             .then(data => this.setState({ data: data }));
+        fetch(`/person/${id}/images`)
+            .then(response => response.json())
+            .then(data => this.setState({ images: data.profiles }));
+        fetch(`/person/${id}/movie_credits`)
+            .then(response => response.json())
+            .then(data => this.setState({ movie_credits: data.cast }));
     }
 
     render() {
+        const { images, movie_credits } = this.state;
         const {
-            name
+            name,
+            biography,
         } = this.state.data;
 
         return (
-            <h1>Name: {name}</h1>
+            <div className="person-container">
+                {images.length > 0 &&
+                    <div className="headshot">
+                        <img src={movieDBImgSource(images[0].file_path)} alt={name} />
+                    </div>
+                }
+                <div className="details">
+                    <h1>{name}</h1>
+                    <p>{biography}</p>
+                    <h3>Movies</h3>
+                    <ul className="movies">
+                        {movie_credits.map(movie => (
+                            <li>
+                                <Link to={location => `/movie/${movie.id}`}><b>{movie.original_title}</b></Link> {movie.character}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
